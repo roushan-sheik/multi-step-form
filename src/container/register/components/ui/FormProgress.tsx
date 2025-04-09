bc2ae27824d9c68f6ffd96e350450a554f5fa485
roushan-sheik <roushansheik@gmail.com>
2025-04-09 22:50:42 +0600

    [style]: update form progress

--- src/container/register/components/ui/FormProgress.tsx
+++ src/container/register/components/ui/FormProgress.tsx
@@ -8,9 +8,9 @@ const FormProgress: FC<TFormProgressProps> = ({ progress }) => {
   const isComplete = progress === "100%";
 
   return (
-    <div className="w-full h-[30px] rounded-[33px] my-8 bg-[#e6e6e6] overflow-hidden">
+    <div className="w-full h-[20px] rounded-[33px] my-8 bg-[#e6e6e6] overflow-hidden">
       <div
-        className={`h-[30px] rounded-[33px] transition-all duration-300 ease-in-out ${
+        className={`h-[20px] rounded-[33px] transition-all duration-300 ease-in-out ${
           isComplete ? "bg-green-500" : "bg-[#f43676]"
         }`}
         style={{ width: progress }}
